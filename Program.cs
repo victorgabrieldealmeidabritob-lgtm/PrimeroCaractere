967f4b83f96c74684440bb308ea15f1533b5f024
Victor Gabriel <victorgabrieldealmeidabritob@gmai.com>
2026-09-23 01:43:40 -0300

    finalizar tarefa

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("--- Primero Caractere ---\n");
+
+Console.Write("Digite algo...: ");
+string textoDigitado = Console.ReadLine()!;
+
+Console.WriteLine(textoDigitado.Trim().Substring(0, 1));
+
+/*estou com duvida nessa parte aqui professor, esta um pouco confuso mas vou praticar para tentar entender direito*/
+
